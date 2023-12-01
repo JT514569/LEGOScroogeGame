5d1bd5f33fdad23cd8f2e40d7a93c509a7c6b117
JT514569 <104973800+JT514569@users.noreply.github.com>
2023-12-01 16:11:10 +0000

    Changes before planning to switch versions

--- Assets/Problems.cs
+++ Assets/Problems.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Problems : MonoBehaviour
 {
-    public int SolvedProblems = 0;
+    public static int SolveProblems;
+    public Text probText;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        string ptext = "Problems: " + SolveProblems;
+        probText.text = ptext;
     }
 }
