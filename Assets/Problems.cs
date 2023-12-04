cf7d54cedaefc2d8be177e9d875907e726e131cb
JT514569 <104973800+JT514569@users.noreply.github.com>
2023-12-04 10:13:08 +0000

    Trying to revert

--- Assets/Problems.cs
+++ Assets/Problems.cs
@@ -1,22 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Problems : MonoBehaviour
 {
-    public static int SolveProblems;
-    public Text probText;
+    public int SolvedProblems = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        string ptext = "Problems: " + SolveProblems;
-        probText.text = ptext;
+        
     }
 }
